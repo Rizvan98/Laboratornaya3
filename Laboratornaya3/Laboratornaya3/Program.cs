49f0e2ffca3999308301e749c63e0d085194e69c
Rizvan98 <rizvan.98@mail.ru>
2020-10-13 17:16:53 +0600

    Лабораторная 3

--- Laboratornaya3/Laboratornaya3/Program.cs
+++ Laboratornaya3/Laboratornaya3/Program.cs
@@ -6,7 +6,19 @@ namespace Laboratornaya3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool Stop = true;
+            int chislo;
+
+            while(Stop)
+            {
+                Console.WriteLine("Введите любое число из этого примера J = - sin^3(y)/1*3 + sin^5(x^2)/3*5 - sin^7(y^3)/5*7+sin^9(x^4)/7*9 >");
+                chislo = Convert.ToInt32(Console.ReadLine());
+
+                if (chislo == 0)
+                    break;
+                if (chislo == 6)
+                    break;
+            }
         }
     }
 }
